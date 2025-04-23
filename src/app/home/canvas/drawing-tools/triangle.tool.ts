a8082b4a6bab1207520f70fe9208ef9155ba3259
kimrenny <sevdon4@gmail.com>
2025-04-24 00:16:06 +0300

    feat: implement triangle drawing logic

--- src/app/home/canvas/drawing-tools/triangle.tool.ts
+++ src/app/home/canvas/drawing-tools/triangle.tool.ts
@@ -1,105 +1,106 @@
 import { DrawingTool } from '../interfaces/drawing-tool.interface';
+import { ToolContext } from '../interfaces/tool-context.interface';
 
 export class Triangle implements DrawingTool {
-  public points: { x: number; y: number }[] = [];
-  private triangles: { x: number; y: number }[][] = [];
-  private previewPoint: { x: number; y: number } | null = null;
+  private path: { x: number; y: number }[] = [];
+  private isDrawing: boolean = false;
+  private end: { x: number; y: number } | null = null;
 
-  addPoint(point: { x: number; y: number }): void {
-    this.previewPoint = null;
-    if (this.points.length < 3) {
-      this.points.push(point);
-    }
-
-    if (this.points.length === 3) {
-      this.triangles.push([...this.points]);
-      this.reset();
-    }
-  }
-
-  setPreviewPoint(point: { x: number; y: number } | null): void {
-    this.previewPoint = point;
-  }
-
-  draw(
-    ctx: CanvasRenderingContext2D,
-    path: { x: number; y: number }[],
-    color: string
-  ): void {
-    this.triangles.forEach((triangle) => {
-      ctx.save();
-      ctx.strokeStyle = color;
-      ctx.lineWidth = 2;
-
+  draw(ctx: CanvasRenderingContext2D): void {
+    if (this.path.length === 3) {
       ctx.beginPath();
-      ctx.moveTo(triangle[0].x, triangle[0].y);
-      ctx.lineTo(triangle[1].x, triangle[1].y);
-      ctx.lineTo(triangle[2].x, triangle[2].y);
+      ctx.moveTo(this.path[0].x, this.path[0].y);
+      ctx.lineTo(this.path[1].x, this.path[1].y);
+      ctx.lineTo(this.path[2].x, this.path[2].y);
       ctx.closePath();
       ctx.stroke();
-
-      ctx.restore();
-    });
-
-    if (this.points.length > 0) {
-      ctx.save();
-      ctx.strokeStyle = color;
-      ctx.lineWidth = 2;
-
-      ctx.beginPath();
-      ctx.moveTo(this.points[0].x, this.points[0].y);
-
-      if (this.points.length >= 2) {
-        ctx.lineTo(this.points[1].x, this.points[1].y);
-      }
-
-      if (this.points.length === 3) {
-        ctx.lineTo(this.points[2].x, this.points[2].y);
-        ctx.closePath();
-      }
-      ctx.stroke();
-      ctx.restore();
-    }
-
-    if (this.previewPoint) {
-      ctx.save();
-      ctx.strokeStyle = this.getPreviewColor(color);
-      ctx.lineWidth = 1;
-
-      ctx.beginPath();
-      ctx.moveTo(this.points[0].x, this.points[0].y);
-
-      if (this.points.length === 1) {
-        ctx.lineTo(this.previewPoint.x, this.previewPoint.y);
-      } else if (this.points.length === 2) {
-        ctx.lineTo(this.points[1].x, this.points[1].y);
-        ctx.lineTo(this.previewPoint.x, this.previewPoint.y);
-        ctx.lineTo(this.points[0].x, this.points[0].y);
-      }
-      ctx.stroke();
-
-      ctx.restore();
     }
   }
 
-  reset(): void {
-    this.points = [];
-    this.previewPoint = null;
+  onMouseDown(pos: { x: number; y: number }, data: ToolContext): void {
+    if (this.path.length < 3) {
+      this.path.push({ x: pos.x, y: pos.y });
+      this.isDrawing = true;
+      this.renderPreview(data);
+    }
   }
 
-  clear(): void {
-    this.triangles = [];
-    this.points = [];
-    this.previewPoint = null;
+  onMouseMove(pos: { x: number; y: number }, data: ToolContext): void {
+    if (this.isDrawing) {
+      this.end = { x: pos.x, y: pos.y };
+      this.renderPreview(data);
+    }
   }
 
-  private getPreviewColor(color: string): string {
-    const opacity = 0.5;
-    return (
-      color +
-      Math.floor(opacity * 255)
-        .toString(16)
-        .padStart(2, '0')
-    );
+  onMouseUp(pos: { x: number; y: number }, data: ToolContext): any {
+    if (this.isDrawing && this.path.length === 1) {
+      this.renderPreview(data);
+    } else if (this.isDrawing && this.path.length === 2) {
+      this.renderPreview(data);
+    } else if (this.isDrawing && this.path.length === 3) {
+      const savePath = [...this.path];
+      this.path = [];
+      this.isDrawing = false;
+      this.end = null;
+
+      return { tool: this, path: savePath };
+    }
+  }
+
+  private renderPreview(data: ToolContext): void {
+    if (!this.isDrawing || !this.end || this.path.length === 0) return;
+
+    const ctx = data.previewCanvas?.getContext('2d');
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, data.previewCanvas.width, data.previewCanvas.height);
+
+    if (this.path.length === 1) {
+      ctx.beginPath();
+      ctx.moveTo(this.path[0].x, this.path[0].y);
+      ctx.lineTo(this.end.x, this.end.y);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    if (this.path.length === 2) {
+      ctx.beginPath();
+      ctx.moveTo(this.path[0].x, this.path[0].y);
+      ctx.lineTo(this.path[1].x, this.path[1].y);
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(this.path[1].x, this.path[1].y);
+      ctx.lineTo(this.end.x, this.end.y);
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(this.path[0].x, this.path[0].y);
+      ctx.lineTo(this.end.x, this.end.y);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    if (this.path.length === 3) {
+      ctx.beginPath();
+      ctx.moveTo(this.path[0].x, this.path[0].y);
+      ctx.lineTo(this.path[1].x, this.path[1].y);
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(this.path[1].x, this.path[1].y);
+      ctx.lineTo(this.path[2].x, this.path[2].y);
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(this.path[2].x, this.path[2].y);
+      ctx.lineTo(this.path[0].x, this.path[0].y);
+      ctx.stroke();
+      ctx.closePath();
+    }
   }
 }
